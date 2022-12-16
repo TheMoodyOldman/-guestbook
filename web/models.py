e59dc0d822f3a71503535614ae389db2f622e2fe
TheMoodyOldman <anthonyfu2005@gmail.com>
2022-12-16 11:18:31 +0800

    new model Message

--- web/models.py
+++ web/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    user = models.CharField('使用者名稱', max_length=20)
+    subject = models.CharField('內容主旨', max_length=50)
+    content = models.TextField('內容')
+    time = models.DateTimeField('', auto_now_add=True)
+
+    def __str__(self):
+        return self.user + ':' + self.subject
